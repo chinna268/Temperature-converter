be194149780167221a8c1c1a16744d3e1475b9ef
Keshav Reddy <keshavreddy8899@gmail.com>
2021-05-10 00:12:10 +0530

    fifth Commit

---
+++ Temperature.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Temperature
+{
+	public static void main(String args[])
+	{
+		double fah,cel;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Choose Type of conversion \n 1.Fahrenheit to Celsius \n 2. Celsius  to  Fahrenheit ");
+		int ch=sc.nextInt();
+		switch(ch)
+		{
+			case 1:
+			System.out.print("Enter Fahrenheit temperature:  ");
+			fah=sc.nextDouble();
+			double con=(fah-32)*5/9;
+			System.out.println("Celsius temperature is: "+con);
+			break;
+			
+			case 2:
+			System.out.print("Enter Celsius temperature:  ");
+			cel=sc.nextDouble();
+			double f=((9*cel)/5)+32;
+			System.out.println("Fahrenheit temperature is: "+f);
+			break;
+			
+			default:
+			System.out.print("Please choose a valid input ");
+			
+			
+			
+			
+			
+		}
+		
+	}
+}
